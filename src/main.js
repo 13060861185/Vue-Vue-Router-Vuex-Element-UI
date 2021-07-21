6ea69771900a23812a2b64e8da68183123cb81a3
zhangby <1607200941@qq.com>
2021-07-21 17:22:54 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -2,8 +2,65 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import './plugins/element.js'
+import {getStore} from '../src/utils/storage'
 
+
+// 使用vue-lazylocad
+import VueLazyLoad from 'vue-lazyload';
+Vue.use(VueLazyLoad);
+
+Vue.use(VueLazyLoad, {
+  preLoad: 1.3,
+  error:'../public/static/images/error.png',
+  loading: '../public/static/images/load.gif',
+  attempt:1
+})
 Vue.config.productionTip = false
+// 挂载到axios到vue的原型，由于继承性，所有的组件都可以使用this.$http
+import axios from 'axios';
+Vue.prototype.$http = axios;
+axios.interceptors.request.use(config => {
+  const token = getStore('token');
+  if(token) {
+    // 表示用户已登录
+    config.headers.common['Authorization'] = token;
+  }
+  return config
+},error => {
+  return Promise.reject(error);
+})
+// 守卫
+router.beforeEach((to, from, next) => {
+
+  axios.post('/api/validate',{}).then(res => {
+    let data = res.data;
+    if(data.state !== 1){
+      if(to.matched.some(record => record.meta.auth)){
+        // 用户未登录 需要跳转登录页面
+        next({
+          path: '/login',
+          query:{
+            redirect:to.fullPath
+          }
+        })
+      }else{
+        next();
+      }
+    }else{
+      // 保存用户的信息
+      store.commit('ISLOGIN',data);
+      if(to.path ==='/login') {
+        router.push({
+          path: '/'
+        })
+      }
+    }
+  }).catch(error => {
+    console.log(error);
+  })
+  next();
+})
 
 new Vue({
   router,
--- src/router.js
+++ src/router.js
@@ -1,23 +1,73 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router';
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+// import Index from './views/Index.vue'
+// import Login from './views/Login/index.vue'
+// import Home from './views/Home/index.vue'
+// import Goods from './views/Goods/index.vue'
+// import Thanks from './views/Thanks/index.vue'
+// import GoodsDetail from './views/GoodsDetail/index.vue'
+// import User from './views/User/index.vue'
+
+// 解决路由命名冲突的方法
+const routerPush = VueRouter.prototype.push
+VueRouter.prototype.push = function push(location) {
+  return routerPush.call(this,location).catch(error => error)
+}
+
+const Index = () => import('./views/Index.vue');
+const Login = () => import('./views/Login/index.vue');
+const Home = () => import('./views/Home/index.vue');
+const Goods = () => import('./views/Goods/index.vue');
+const Thanks = () => import('./views/Thanks/index.vue');
+const GoodsDetail = () => import('./views/GoodsDetail/index.vue');
+const User = () => import('./views/User/index.vue');
 
 Vue.use(Router)
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: Home
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+const routes = [
+  {
+    path: '/',
+    redirect:'/home',
+    name:'home',
+    component: Index,
+    children:[
+      {
+        path:'home',
+        component:Home
+      },
+      {
+        path:'goods',
+        component:Goods
+      },
+      {
+        path: 'thanks',
+        component:Thanks
+      },
+      {
+        path: 'goodsDetail',
+        name: 'goodsDetail',
+        component: GoodsDetail
+      }
+    ]
+  },
+  {
+    path: '/login',
+    name: "login",
+    component:Login
+  },
+  {
+    path: '/user',
+    name: "user",
+    component: User,
+    meta:{
+      // 需要守卫
+      auth:true
     }
-  ]
+  }
+]
+
+export default new Router({
+  mode:'history',
+  routes
 })
